9e298f163a1a7782851211a7067faa92ae2aab28
Héricles <hericles@getupcloud.com>
2022-08-22 11:23:02 -0300

    chg: feat: seo meta description tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
 import React, { useEffect } from "react";
 import Layout from "@theme/Layout";
+import Head from "@docusaurus/Head";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import HubspotForm from "../components/HubspotForm";
-import Translate from '@docusaurus/Translate';
-import {FaArrowRight} from "react-icons/fa";
+import Translate, { translate } from "@docusaurus/Translate";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
-
   return (
-    <Layout description="UnDistro is a vanilla, non-opinionated, and open source Kubernetes distribution">
+    <Layout>
+      <Head>
+        <meta
+          name="description"
+          content={translate({
+            id: "description",
+            message:
+              "Visualize, crie e gerencie um ou mais clusters de forma padronizada e centralizada. Obtenha acesso antecipado ao produto.",
+          })}
+        />
+      </Head>
       <main className="bg-dark">
         <div className="bg-[url(/img/background.jpg)] bg-top bg-cover bg-no-repeat min-h-screen">
           <div className="flex flex-col w-full h-fit items-center justify-center space-y-24 py-24 mx-auto max-w-container 2xl:max-w-7xl">
@@ -18,16 +28,18 @@ export default function Home() {
               alt="UnDistro"
             />
             <h1 className="text-primary text-4xl lg:text-5xl uppercase font-black text-center xl:w-2/3">
-              <Translate id="heading">Know your Kubernetes clusters inside out</Translate>
+              <Translate id="heading">
+                Know your Kubernetes clusters inside out
+              </Translate>
             </h1>
             <h2 className="text-primary text-xl md:text-2xl lg:text-3xl font-light md:w-2/3 text-center">
               <Translate id="subtitle">
-              Manage, spin up and visualize in a standardized and centralized
-              way, one or more clusters.
+                Manage, spin up and visualize in a standardized and centralized
+                way, one or more clusters.
               </Translate>
             </h2>
             <HubspotForm />
-            </div>
+          </div>
         </div>
       </main>
     </Layout>
